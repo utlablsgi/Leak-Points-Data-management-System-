b6c4fefad7217cb18ed4e40da39214579f7700f1
utlablsgi <96718966+utlablsgi@users.noreply.github.com>
2022-03-26 11:31:08 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -456,7 +456,7 @@ elif genre == 'LNC':
         a    {font-family: Sans-serif;}
     </style>
          <p>Click on the image to download it:<p>
-<a href="/Create trial.txt" download>
+<a href="https://filesamples.com/samples/document/txt/sample3.txt" download>
         <img src="https://1000logos.net/wp-content/uploads/2021/04/MATLAB-logo.png" alt="W3Schools" width="104" height="142">
  
 </a>
