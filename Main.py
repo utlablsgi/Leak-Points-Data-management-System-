4052099a385ca51bcec433431554117fd77c1dd7
utlablsgi <96718966+utlablsgi@users.noreply.github.com>
2022-04-02 11:53:43 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -451,7 +451,8 @@ elif genre == 'Mathlab code download':
      with st.container():
                 
         with open('PAR_inc_loop_2020.m') as f:
-                st.download_button('Download mathlab code', f, file_name='Random Number Generator.mlappinstall') 
+                st.download_button('Download mathlab code', f, file_name=['Random Number Generator.mlappinstall','Random Number Generator.prj']) 
+                
         
      
         
